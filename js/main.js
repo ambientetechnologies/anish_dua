f52da3811dc4443285d8de7d2628b86c8065cbef
Nimit-Sachdeva <nimitsachdeva13@gmail.com>
2018-03-13 15:34:36 +0530

    Responsive done

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function show() {
+    document.getElementById('left').className +=' col-xs-12 col-md-6 col-lg-6 col-sm-6';
+    document.getElementById('left').style.backgroundSize='cover';
+    document.getElementById('left').style.backgroundImage="url('images/anish.png')";
+    document.getElementById('left').style.backgroundPosition="left";
+    document.getElementById('right').style.display= 'block';
+    document.getElementById("transparent").style.display='none';
+}
